5266265d054ff4d530b4d5848827477033705bcc
AndrijaSkontra <askontra22@student.unizd.hr>
2024-02-03 13:27:42 +0100

    added error page, fixed some layout problems

--- app.py
+++ app.py
@@ -1,23 +1,18 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from api_handler import APIHandler  # Uvozimo APIHandler klasu iz vanjske datoteke
 
 app = Flask(__name__)
 
-# Route za početnu stranicu
 @app.route("/home")
 def home():
     return render_template("home.html")
 
-# Route za vremenske podatke
 @app.route("/weather", methods=["GET", "POST"])
 def weather():
     data = None
     if request.method == "POST":
-        # Kreiramo instancu APIHandler klase s gradom dobivenim iz obrasca
         api_class = APIHandler(request.form["cityName"])
-        # Pozivamo metodu za dohvaćanje vremenskih podataka iz APIHandler klase
         data = api_class.get_weather_data()
-    # Prikazujemo template "weather.html" s podacima
     return render_template("weather.html", data=data)
 
 # Route za podatke o vjetru
@@ -25,13 +20,17 @@ def weather():
 def wind():
     data = None
     if request.method == "POST":
-        # Kreiramo instancu APIHandler klase s gradom dobivenim iz obrasca
         api_class = APIHandler(request.form["cityName"])
-        # Pozivamo metodu za dohvaćanje podataka o vjetru iz APIHandler klase
         data = api_class.get_wind_data()
-    # Prikazujemo template "wind.html" s podacima
     return render_template("wind.html", data=data)
 
-# Ako se aplikacija pokreće izravno, pokrećemo Flask aplikaciju
+@app.route('/')
+def redirectToHome():
+    return redirect("/home")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
